3e73fb74b33ad563f27296fac5f8907e233227f9
espy02 <82273962+espy02@users.noreply.github.com>
2023-12-07 09:37:42 +0000

    optimize code

--- advent_of_code/2015/day_6/part_one.py
+++ advent_of_code/2015/day_6/part_one.py
@@ -5,37 +5,24 @@ file.close()
 grid = {}
 
 for instruction in data:
-    action = ""
-    through = instruction.find("through")
-    left = []
-    right = instruction[through + 8:].split(",")
-    if instruction.find("on") != -1:
-        action = "on"
-        left = instruction[8:through - 1].split(",")
-    elif instruction.find("off") != -1:
-        action = "off"
-        left = instruction[9:through - 1].split(",")
-    elif instruction.find("toggle") != -1:
-        action = "toggle"
-        left = instruction[7:through - 1].split(",")
+    instruction = instruction.split(" ")
+    left = instruction[2 - (5 % len(instruction))].split(",")
+    right = instruction[4 - (5 % len(instruction))].split(",")
 
     for i in range(int(left[0]), int(right[0]) + 1):
         for j in range(int(left[1]), int(right[1]) + 1):
             coordinates = str(i) + "_" + str(j)
             if coordinates not in grid:
                 grid[coordinates] = False
-            if action == "on":
+            if "on" in instruction:
                 if not grid[coordinates]:
                     grid[coordinates] = True
-            if action == "off":
+            if "off" in instruction:
                 if grid[coordinates]:
                     grid[coordinates] = False
-            if action == "toggle":
+            if "toggle" in instruction:
                 grid[coordinates] = not grid[coordinates]
 
-lights_lit = 0
-for on in grid.values():
-    if on:
-        lights_lit += 1
+lights_lit = sum(grid.values())
 
 print("Lights lit:", lights_lit)  # Lights lit: 569999
--- advent_of_code/2015/day_6/part_two.py
+++ advent_of_code/2015/day_6/part_two.py
@@ -5,37 +5,25 @@ file.close()
 grid = {}
 
 for instruction in data:
-    action = ""
-    through = instruction.find("through")
-    left = []
-    right = instruction[through + 8:].split(",")
-    if instruction.find("on") != -1:
-        action = "on"
-        left = instruction[8:through - 1].split(",")
-    elif instruction.find("off") != -1:
-        action = "off"
-        left = instruction[9:through - 1].split(",")
-    elif instruction.find("toggle") != -1:
-        action = "toggle"
-        left = instruction[7:through - 1].split(",")
+    instruction = instruction.split(" ")
+    left = instruction[2 - (5 % len(instruction))].split(",")
+    right = instruction[4 - (5 % len(instruction))].split(",")
 
     for i in range(int(left[0]), int(right[0]) + 1):
         for j in range(int(left[1]), int(right[1]) + 1):
             coordinates = str(i) + "_" + str(j)
             if coordinates not in grid:
                 grid[coordinates] = False
-            if action == "on":
+            if "on" in instruction:
                 grid[coordinates] += 1
-            if action == "off":
+            if "off" in instruction:
                 if grid[coordinates] - 1 >= 0:
                     grid[coordinates] -= 1
                 else:
                     grid[coordinates] = 0
-            if action == "toggle":
+            if "toggle" in instruction:
                 grid[coordinates] += 2
 
-total_brightness = 0
-for brightness in grid.values():
-    total_brightness += brightness
+total_brightness = sum(grid.values())
 
 print("Total brightness:", total_brightness)  # Total brightness: 17836115
